36b03a6c0605f2ef2b1816007492cc61ac23b0f7
Dmitriy-Yakovlev-JS <julidima75@gmail.com>
2023-05-16 01:49:34 +0300

    update

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -34,4 +34,9 @@ export class ReviewController {
   ) {
     return this.reviewService.create(id, dto, +productId)
   }
+
+  @Get("average-by-product/:productId")
+  async getAverageByProduct(@Param("productId") productId: string) {
+    return this.reviewService.getAverageValueByProductId(+productId);
+  }
 }
